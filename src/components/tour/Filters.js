e60adbd8c2fb5ae7d46f4ec2d8ab268e737c8b24
Swaroop Uprety <breadfan18@gmail.com>
2024-01-08 18:39:09 -0500

    welll baseline

--- src/components/tour/Filters.js
+++ src/components/tour/Filters.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { ERN_DATA_KEYS } from "../../constants/constants";
 
-function Filters({ dataFilter, handleDataFilter }) {
+function Filters({ dataFilter, handleDataFilter, dataType }) {
   return (
     <>
       <input
         type="search"
         value={dataFilter.query}
-        onChange={(e) => handleDataFilter(e, ERN_DATA_KEYS.tourName)}
-        placeholder="Filter by tour name.."
+        onChange={(e) => handleDataFilter(e, dataType)}
+        placeholder={`Filter by ${dataType}`}
         className="tourTabsFilterInput"
         // style={{ width: filterWidth }}
       />
